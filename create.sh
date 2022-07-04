21cedb241f890cd7cc671464e12e3c0a77cbf78b
Alexander Wesolowski <foglerek@gmail.com>
2022-07-04 11:00:36 -0700

    misc changes

---
+++ create.sh
@@ -0,0 +1,6 @@
+ts-node ./js/packages/cli/src/candy-machine-v2-cli.ts upload \
+    -e devnet \
+    -k ~/.config/solana/devnet.json \
+    -cp ./dev/config.json \
+    -c example2 \
+    ./dev/assets
---
+++ update.sh
@@ -0,0 +1,5 @@
+ts-node ./js/packages/cli/src/candy-machine-v2-cli.ts update_candy_machine \
+    -e devnet \
+    -k ~/.config/solana/devnet.json \
+    -cp ./dev/config.json \
+    -c example2
